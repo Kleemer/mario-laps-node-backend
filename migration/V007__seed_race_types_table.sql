51d907bdb992835ff1be381451616b501a876694
Yassine Ammar <yassine@whispli.com>
2022-03-24 13:08:28 +0100

    feat: seed new race types from DLC

---
+++ migration/V007__seed_race_types_table.sql
@@ -0,0 +1,9 @@
+# Added in Booster Course Pass DLC First Wave in March 18th 2022
+INSERT INTO `raceTypes` (id, name) VALUES ('paris-promenade', 'Paris Promenade');
+INSERT INTO `raceTypes` (id, name) VALUES ('toad-circuit', 'Toad Circuit');
+INSERT INTO `raceTypes` (id, name) VALUES ('choco-mountain', 'Choco Mountain');
+INSERT INTO `raceTypes` (id, name) VALUES ('coconut-mall', 'Coconut Mall');
+INSERT INTO `raceTypes` (id, name) VALUES ('tokyo-blur', 'Tokyo Blur');
+INSERT INTO `raceTypes` (id, name) VALUES ('shroom-ridge', 'Shroom Ridge');
+INSERT INTO `raceTypes` (id, name) VALUES ('sky-garden', 'Sky Garden');
+INSERT INTO `raceTypes` (id, name) VALUES ('ninja-hideaway', 'Ninja Hideaway');
